aa8c95dccf1a575732da3b50b7fbfd3a3f7b4ee2
akrishnan93 <anand.krrishnan@gmail.com>
2020-09-26 12:26:17 -0400

    Deleted unecessary files

---
+++ android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/objectdetector/ObjectDetectorTest.java
@@ -0,0 +1,16 @@
+package com.google.mlkit.vision.demo.objectdetector;
+
+import com.javatpoint.logic.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ObjectDetectorTest {
+
+    @Test
+    public void testObjectDetectorPriorityQueue() {
+        assertEquals("121", "121"); 
+    }   
+
